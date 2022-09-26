7d730b58ccf01252a8f5a0ed6a135172a7d3e67c
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-09-26 18:39:00 +0600

    Calculations added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,38 @@
 import React from "react";
-
+import "./Cart.css";
 const Cart = (props) => {
-    const {cart}= props;
-    // console.log(cart);
+  const { cart } = props;
+
+  let totalPrice = 0,
+    shippingCost = 0;
+
+  for (const product of cart) {
+    console.log(product);
+    totalPrice += product.price;
+    shippingCost += product.shipping;
+  }
+  let tax = (totalPrice * 0.1).toFixed(2);
+
+  let grandTotal=totalPrice+shippingCost+parseFloat(tax);
+
+  // console.log(cart);
   return (
-    <div>
+    <div className="carts-container">
       <h2>Order Summery</h2>
       <p>
         Selected Items <b>{cart.length}</b>
       </p>
+      <p>
+        Total price <b>$ {totalPrice}</b>
+      </p>
+      <p>
+        Total shiping: <b>$ {shippingCost}</b>
+      </p>
+      <p>
+        Tax: <b>$ {tax}</b>
+      </p>
+
+      <h3>Grand Total:{grandTotal}</h3>
     </div>
   );
 };
